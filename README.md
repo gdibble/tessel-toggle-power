2ce0f279bd4705224600ba489491c09fc27af0de
Gabriel Dibble <gdibble@gmail.com>
2016-05-22 22:35:08 -0700

    0.2.0

--- README.md
+++ README.md
@@ -6,7 +6,9 @@
 ```javascript
 var togglePowerConfig = { use:'pin', pin:pin, pinNum:7 };
 var togglePower = require('tessel-toggle-power').bind(togglePowerConfig);
-togglePower();  // call it
+togglePower();   // Toggle power on a pin or relay
+togglePower(1);  // or togglePower(true)   // Manually set power On
+togglePower(0);  // or togglePower(false)  // Manually set power Off
 ```
 
 #### togglePowerConfig Options which get bound to fn as shown above^
@@ -19,3 +21,32 @@ togglePower();  // call it
   debug:    false    // pass: true or false
 }
 ```
+
+#### Example shell output from setting ^option `debug:true`
+
+* From running `togglePower(1)` or `togglePower(true)`:
+  ```
+  
+  togglePower:
+  * Setting pin to 1
+  * Pin set to 1    2016-05-23T04:17:26.259Z
+  
+  ```
+
+* From running `togglePower(0)` or `togglePower(false)`:
+  ```
+  
+  togglePower:
+  * Setting pin to 0
+  * Pin set to 0    2016-05-23T04:17:26.674Z
+  
+  ```
+
+* From running `togglePower()`:
+  ```
+
+  togglePower:
+  * Toggling relay
+  * Relay toggled
+
+  ```
--- index.js
+++ index.js
@@ -6,7 +6,9 @@
  *  - First set a var for your `pin` or `relay` then try this example:
  *      var togglePowerConfig = { use:'pin', pin:pin, pinNum:7 };
  *      var togglePower = require('tessel-toggle-power').bind(togglePowerConfig);
- *      togglePower();  // call it
+ *      togglePower();   // Call it without an argument to toggle
+ *      togglePower(1);  // or togglePower(true);   // Manually set power On
+ *      togglePower(0);  // or togglePower(false);  // Manually set power Off
  *
  *  - togglePowerConfig Options which get bound to fn as shown above^
  *    {
@@ -18,42 +20,89 @@
  *    }
  */
 
-var togglePower = function togglePower() {
-  var onErr = function onErr(msg) {
-    console.log('\n\ntessel-toggle-power error: ', msg);
-    console.log('\n  config:\n', this);
-    console.log('\n  ' + getDate(), '\n');
-    return new Error(msg);
-  }.bind(this);
+var togglePower = function togglePower(state) {
+  var deflt = 'relay';  // Default for `use` setting
+
+  this.state = state;
+  this.toggleMode = state === undefined;
 
   if (!(this instanceof Object)) {
-    onErr('config scope passed is not an object');
+    onErr.call(this, 'config scope passed is not an object');
   }
 
-  if (this.relay && (this.use === 'relay' || !this.use)) {
-    this.relay.toggle(this.relayChn);
-    this.debug ? console.log('\nRelay toggled', getDate()) : null;
+  this.debug && !this.toggleMode ? // Debug Output
+    console.log('\ntogglePower:\n* ' + (this.toggleMode ? 'Toggling ' : 'Setting ') + (this.use ? this.use : deflt) + (this.toggleMode ? '' : (' to ' + Number(!!this.state)))) :
+    null;
+
+  if (this.relay && (this.use === deflt || !this.use)) {
+    // Control Relay
+    this.toggleMode ?
+      this.relay.toggle(this.relayChn, relayChangeCallback.bind(this)) : // Toggle relay
+      this.relay[!!state ? 'turnOn' : 'turnOff'](this.relayChn, relayChangeCallback.bind(this)); // Manual mode
+
+    this.debug ? // Debug Output
+      console.log('* Relay ' + (this.toggleMode ? 'toggled' : ('set to ' + Number(!!state))), getDate()) :
+      null;
+
   } else if (this.pin) {
-    this.pin.read(pinReadCallbackToToggle.bind(this));
-    this.debug ? console.log('\nPin toggled', getDate()) : null;
+    // Control Pin
+    this.toggleMode ?
+      this.pin.read(pinReadCallbackToToggle.bind(this)) : // Toggle pin
+      this.pin.output(Number(!!state)); // Manual mode: state => Boolean => Number
+
+    this.debug ? // Debug Output
+      console.log('* Pin ' + (this.toggleMode ? 'toggled' : ('set to ' + Number(!!state))), getDate()) :
+      null;
+
   } else {
-    onErr('neither relay or pin defined');
+    // Fail
+    onErr.call(this, 'neither relay or pin defined');
   }
 
   return;
 };
 
+
+// Relay turnOn/turnOff Callback
+var relayChangeCallback = function relayChangeCallback(error) {
+  error ? onErr.call(this, 'relay change' + error) : null;
+};
+
+
 // Pin Read Callback which performs toggling mechanism in pin-mode
 // * when 0 => turn On, when 1 => turn Off
 var pinReadCallbackToToggle = function pinReadCallbackToToggle(error, value) {
-  var inverted = value ? 0 : 1;                                  // Invert value
-  this.debug ? console.log('\npin.read()  =>', value) : null;    // Debug Output
-  this.pin.output(inverted);                                     // Set new value
-  this.debug ? console.log('\npin set     =>', inverted) : null; // Debug Output
+  var inverted                  = value ? 0 : 1; // Invert value
+  var stateBoolean              = !!this.state;
+  var valueBoolean              = !!value;
+  var toggleModeAndValuesAlike  = this.toggleMode && stateBoolean === valueBoolean;
+  var toggleModeAndValuesDiffer = this.toggleMode && stateBoolean !== valueBoolean;
+
+  this.debug ? console.log('\npin.read()  =>', value) : null; // Debug Output
+
+  if (!this.toggleMode || toggleModeAndValuesDiffer) {
+    this.pin.output(inverted); // Set new value
+    this.debug ? console.log('\npin set     =>', inverted) : null; // Debug Output
+  } else if (this.debug && toggleModeAndValuesAlike) {
+    console.log('not setting', Number(stateBoolean), 'to the same value', Number(valueBoolean)); // Debug Output
+  }
+
+  return;
 };
 
-// Gets an ISO formattted date
-var getDate = function getDate() { return new Date().toISOString(); };
 
-// Export module
+// General error output
+var onErr = function onErr(msg) {
+  console.log('\n\ntessel-toggle-power error: ', msg);
+  console.log('\n  config:\n', this);
+  console.log('\n  ' + getDate(), '\n');
+  return new Error(msg);
+};
+
+
+// Gets an ISO formattted date
+var getDate = function getDate() { return '   ' + new Date().toISOString(); };
+
+
+// Export fn
 module.exports = togglePower;
